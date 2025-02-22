ace84d56ca1049c28607a89d1549c7d04f5a89cf
KuhleNtshona <kurra.ntshona@gmail.com>
2025-02-23 00:51:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Get the button element 
+const changeTextBtn =
+document.getElementById("changeTextBtn");
+
+// Add and event listener for the button click
+changeTextBtn.addEventListener("click", function() { 
+
+// Get the section to change text 
+const introText = document.getElementById("intro");
+
+// Change the text content 
+introText.innerHTML = '<h2>Thanks for clicking the button!</h2><p>You just interacted with Javescript on this website.</p>';
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
